369dcb565ea8048462b47649a6d8f7886a003da8
Stelios Antoniou <s_Antoniou@yahoo.com>
2021-11-20 11:25:39 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -2,11 +2,13 @@
 $inputfl='LoremIpsum.txt';
 $content='';
 
-
+#get the content of the file
 $content = file_get_contents($inputfl);
 
+#use regex to find the words inside the brackets
 preg_match_all('/\[.*?\]/',$content,$myarr);
 
+#output the results to browser's console
 echo '<script type="text/javascript">console.log('.json_encode($myarr).')</script>';
 
 
